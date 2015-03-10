18670a603e53b64c0774469adde7bd3987c367ae
luc-a91 <luc--a@hotmail.it>
2015-03-10 13:21:43 +0100

    changes1

--- code.cpp
+++ code.cpp
@@ -6,5 +6,11 @@ int main(){
   cout << "hello world\n";
   for(int i=0; i<5; i++)
     cout << "ciao" << endl;
+    
+  int var=0;
+  cout << "write a number: ";
+  cin >> var;
+  cout << "you wrote " << var << endl;
+  
   return 0;
 }
